924532c14a4f16668391fdc25c2d5bb5f03491d5
Adharsh sabu <44582238+raven-97@users.noreply.github.com>
2019-09-15 11:18:06 +0530

    Create UtopianTree.py

---
+++ Miscellanous/UtopianTree.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    res = 1
+    for i in range(n):
+        if i %2 == 0:
+            res = 2*res
+        else:
+            res += 1
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
