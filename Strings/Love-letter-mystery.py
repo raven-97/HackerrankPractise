50fbd9fbc81649fd6f757caed29d088b50522b5b
Adharsh sabu <44582238+raven-97@users.noreply.github.com>
2020-01-04 19:17:34 +0530

    Create Love-letter-mystery.py

---
+++ Strings/Love-letter-mystery.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    rev = ''.join(reversed(s))
+    count =0
+    for i in range(len(s)//2):
+        count += abs(ord(s[i])- ord(rev[i]))
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
